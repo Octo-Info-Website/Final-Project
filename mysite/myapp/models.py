2d5983a8bf44c4200dc3e7eaba36da566f84674a
Kareema Wahid <kareema_wahid@hotmail.com>
2023-09-13 15:36:05 +0100

    Create database models

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
